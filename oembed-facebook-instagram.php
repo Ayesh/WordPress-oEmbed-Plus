5c1459f007c4dc5328f0125c48dd87aa9f48ae8d
Ayesh Karunaratne <ayesh@ayesh.me>
2020-08-08 09:53:06 +0700

    Refactor plugin to use new endpoints

--- oembed-facebook-instagram.php
+++ oembed-facebook-instagram.php
@@ -10,32 +10,25 @@
 
 add_filter('oembed_providers', static function(array $providers): array{
 	return $providers + [
-			'#https?://www\.facebook\.com/.*/posts/.*#i'        => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/.*/activity/.*#i'     => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/.*/photos/.*#i'       => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/photo(s/|\.php).*#i'  => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/permalink\.php.*#i'   => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/media/.*#i'           => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/questions/.*#i'       => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/notes/.*#i'           => [ 'facebook_oembed.post', true ],
-			'#https?://www\.facebook\.com/.*/videos/.*#i'       => [ 'facebook_oembed.video', true ],
-			'#https?://www\.facebook\.com/video\.php.*#i'       => [ 'facebook_oembed.video', true ],
+			'#https?://www\.facebook\.com/.*/posts/.*#i'        => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/.*/activity/.*#i'     => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/.*/photos/.*#i'       => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/photo(s/|\.php).*#i'  => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/permalink\.php.*#i'   => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/media/.*#i'           => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/questions/.*#i'       => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/notes/.*#i'           => [ 'https://graph.facebook.com/v8.0/oembed_post', true ],
+			'#https?://www\.facebook\.com/.*/videos/.*#i'       => [ 'https://graph.facebook.com/v8.0/oembed_video', true ],
+			'#https?://www\.facebook\.com/video\.php.*#i'       => [ 'https://graph.facebook.com/v8.0/oembed_video', true ],
 
-			'#https?://(www\.)?instagr(\.am|am\.com)/(p|tv)/.*#i' => array( 'instagram_oembed.post', true ),
+			'#https?://(www\.)?instagr(\.am|am\.com)/(p|tv)/.*#i' => [ 'https://graph.facebook.com/v8.0/instagram_oembed', true ],
 		];
 });
 
 add_filter('oembed_fetch_url', static function($provider_url): string{
-	switch ($provider_url) {
-		// todo: Attach app access token:
-		case 'facebook_oembed.post':
-			return 'https://graph.facebook.com/v8.0/oembed_post';
-		case 'facebook_oembed.video':
-			return 'https://graph.facebook.com/v8.0/oembed_videos';
-		case 'instagram_oembed.post':
-			return 'https://graph.facebook.com/v8.0/instagram_oembed';
-
-		default:
-			return $provider_url;
+	if (strpos($provider_url, 'https://graph.facebook.com/v8.0/oembed_') === 0) {
+		$provider_url = add_query_arg(['access_token' => oembed_facebook_instagram_get_access_token()], $provider_url);
 	}
+
+	return $provider_url;
 });
