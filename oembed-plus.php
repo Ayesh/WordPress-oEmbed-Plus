708cb183e335e7a09d1be0870a611836fe7796c9
Ayesh Karunaratne <ayesh@ayesh.me>
2020-09-16 11:52:53 +0700

    Fixes #4: Improve the provider check URL

--- oembed-plus.php
+++ oembed-plus.php
@@ -19,12 +19,9 @@ add_filter('oembed_providers', static function(array $providers): array{
 });
 
 add_filter('oembed_fetch_url', static function($provider_url): string{
-	if (
-	    strpos($provider_url, 'https://graph.facebook.com/v8.0/oembed_') === false
-        || strpos($provider_url, 'https://graph.facebook.com/v8.0/instagram_oembed') !== false
-    ) {
-		return $provider_url;
-	}
+	if (strpos($provider_url, 'https://graph.facebook.com/v8.0/') !== 0) {
+        return $provider_url;
+    }
 
 	require_once __DIR__ . '/src/Embed.php';
 	$embed = new Embed(get_option('oembed_facebook_app_id', null), get_option('oembed_facebook_app_secret', null));
